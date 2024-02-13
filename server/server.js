eca0737c62385a95c50953724cbf9f0700309fc5
Cedrick <133091984+cedrick-jsx@users.noreply.github.com>
2024-02-13 20:08:42 +0800

    Update Files

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://cedrick-grocery-app.vercel.app"],
+    methos:["POST"],
+    credentials:true
+  }
+));
 app.use(express.json());
 app.use("/api/user", userRoute);
 
