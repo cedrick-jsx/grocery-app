f62148706ce25d09bac58ca7f67f010d7f59a1bb
Cedrick <133091984+cedrick-jsx@users.noreply.github.com>
2024-02-13 20:34:57 +0800

    Update Files

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json());
 app.use("/api/user", userRoute);
