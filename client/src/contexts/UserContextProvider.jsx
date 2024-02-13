c1c770ca0aa2a3996330ee7a591b2eadc19563c0
Cedrick <133091984+cedrick-jsx@users.noreply.github.com>
2024-02-13 19:56:49 +0800

    Update Files

--- client/src/contexts/UserContextProvider.jsx
+++ client/src/contexts/UserContextProvider.jsx
@@ -4,7 +4,7 @@ import { UserReducer, userStatus } from "../hooks/UserReducer";
 export const UserContext = createContext();
 
 export const UserContextProvider = (props) => {
-  const URL = "http://localhost:4000/api/user/";
+  const URL = "https://cedrick-grocery-app.vercel.app/api/user/";
 
   const [state, dispatch] = useReducer(UserReducer, {
     user: null,
