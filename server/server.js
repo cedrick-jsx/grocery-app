9c5cdecbc7a253e43398d4d1ca65f7f12297b764
Cedrick Cortez <cedrickcirdec@gmail.com>
2024-02-14 17:08:00 +0800

    Update Files

--- server/server.js
+++ server/server.js
@@ -3,11 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute.js");
-const allowCors = require("./middlewares/allowAllCors.js");
 
 const app = express();
 
-app.use(allowCors);
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoute);
