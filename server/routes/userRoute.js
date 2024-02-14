648a7fcb558f062b6ef877d215118f02cd80b668
Cedrick Cortez <cedrickcirdec@gmail.com>
2024-02-14 14:06:18 +0800

    Update Files

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,10 +3,6 @@ const { loginUser, signupUser } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  response.status(200).json({ message: "Server is running" });
-});
-
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
 
--- server/server.js
+++ server/server.js
@@ -1,31 +1,40 @@
 require("dotenv").config();
-// const { createServer } = require("@vercel/node");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute.js");
+const path = require("path");
 
-// const PORT = process.env.PORT;
-// const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 app.use("/api/user", userRoute);
 
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log("Connected to Database");
-//       console.log("Listening on Port", PORT);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+app.get("/api/server", (request, response) => {
+  response.status(200).json({ message: "Server is running" });
+});
 
-mongoose.connect(process.env.MONGO_URI);
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-module.exports = app;
+app.get("*", (request, response) => {
+  try {
+    response.sendFile(path.join(__dirname, "/client/build/index.html"));
+  } catch (err) {
+    response.status(400).send(err);
+  }
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to Database");
+      console.log("Listening on Port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
