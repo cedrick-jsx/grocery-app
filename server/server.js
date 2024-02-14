5cfdf67412e2dbc0e10389ac21d9f958fbe8df45
Cedrick Cortez <cedrickcirdec@gmail.com>
2024-02-14 18:48:49 +0800

    Update Files

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
